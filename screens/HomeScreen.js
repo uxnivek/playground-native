da9e8c5ad61cb3b648ae2cd5697552dbf31e140b
kevinxu <cooldaddy811@gmail.com>
2018-09-29 23:01:31 +1200

    add more modules

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -101,7 +101,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F84B85',
   },
   developmentModeText: {
     marginBottom: 20,
